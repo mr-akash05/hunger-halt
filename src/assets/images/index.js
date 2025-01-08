016d808674f8351f6e62d16ea1d5edb3ebf2fd2c
Akash Gujju <151561916+mr-akash05@users.noreply.github.com>
2025-01-08 11:35:31 +0530

    Add files via upload

---
+++ src/assets/images/index.js
@@ -0,0 +1,21 @@
+import deliver from './home/deliver.png';
+import vector1 from './home/vector1.jpg';
+import donate from './home/donate.jpg';
+import edibleFood from './home/edibleFood.jpg';
+import money from './home/money.jpg';
+import rawFood from './home/rawFood.jpg';
+import happy from './home/happy.jpg'
+import ngo from './home/ngo.jpg'
+import zeroHunger from './home/zeroHunger.jpg'
+import goodHealth from './home/goodHealth.jpg'
+import volunteer from './home/volunteer.jpg'
+import foodDonate from './home/foodDonate.jpg'
+import profile from './home/profile.jpg'
+import google from './login/google.png'
+import email from './login/email.png'
+import loginVector from './login/loginVector.jpg'
+import qrCode from './home/qrCode.png'
+import about from './home/about.jpg'
+
+
+export { about,foodDonate,deliver,profile,vector1,rawFood,edibleFood,donate,money,zeroHunger,goodHealth,happy,ngo,volunteer,google,email,loginVector,qrCode };
